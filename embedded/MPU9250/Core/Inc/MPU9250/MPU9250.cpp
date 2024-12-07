6af6ef0ad93286276090ea6bab68387b62876160
Eirik Kaldhol Strandman <63415376+Eirik2020@users.noreply.github.com>
2024-12-07 20:23:38 +0100

    MPU9250

---
+++ embedded/MPU9250/Core/Inc/MPU9250/MPU9250.cpp
@@ -0,0 +1,18 @@
+/*
+ * MPU9250.cpp
+ *
+ *  Created on: Dec 7, 2024
+ *      Author: xoonz
+ */
+
+#include <MPU9250/MPU9250.h>
+
+MPU9250::MPU9250() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MPU9250::~MPU9250() {
+	// TODO Auto-generated destructor stub
+}
+
